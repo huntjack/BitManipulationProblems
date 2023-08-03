0225b65202fb7816b2c6b7f05dfb58154c99a1ad
huntjack <hunt.jack01@gmail.com>
2023-08-02 19:05:19 -0700

    Improved input validation
    Vertical bounds and width bounds are now checked correctly. Methods were also refactored to improve readability.

--- src/DrawLine.java
+++ src/DrawLine.java
@@ -4,12 +4,16 @@ public class DrawLine {
                                   int x1,
                                   int x2,
                                   int y) {
-        if(screen == null || !isValid(width, x1, x2, y) || isOutOfBounds(width, x1, x2)) {
+        if(screen == null || !isValid(width, x1, x2, y) || isOutOfBoundsHorizontally(width, x1, x2)) {
             throw new IllegalArgumentException();
         }
-        int startIndex = getByteIndex(screen, width, x1, y);
+        int height = getHeight(screen, width);
+        if(isOutOfBoundsVertically(y, height)) {
+            throw new IllegalArgumentException();
+        }
+        int startIndex = getByteIndex(height, width, x1, y);
         int startBit = getBitIndex(x1);
-        int endIndex = getByteIndex(screen, width, x2, y);
+        int endIndex = getByteIndex(height, width, x2, y);
         int endBit = getBitIndex(x2);
         byte startByte = screen[startIndex];
         if(startIndex == endIndex) {
@@ -28,15 +32,40 @@ public class DrawLine {
         }
         return screen;
     }
-    private static int getByteIndex(byte[] screen, int width, int x, int y) {
+    private static boolean isValid(int width, int x1, int x2, int y) {
+        return width >= 8 &&
+                isDivisibleBy(width, 8) &&
+                x1 >= 0 &&
+                x2 >= 0 &&
+                y >= 0;
+    }
+    private static boolean isDivisibleBy(int dividend, int divisor) {
+        return dividend % divisor == 0;
+    }
+    private static boolean isOutOfBoundsHorizontally(int width, int x1, int x2) {
+        return x2 >= width || x2 < x1;
+    }
+    private static int getHeight(byte[] screen, int width) {
         int bytesPerRow = width / 8;
-        int height = screen.length / bytesPerRow;
+        return screen.length / bytesPerRow;
+    }
+    private static boolean isOutOfBoundsVertically(int y, int height) {
+        return y >= height;
+    }
+    private static int getByteIndex(int height, int width, int x, int y) {
+        int bytesPerRow = width / 8;
         int row = (height - 1) - y;
         int indexOfByteInRow = x / 8;
         return row * bytesPerRow + indexOfByteInRow;
     }
     private static int getBitIndex(int x) {
         return x % 8;
+    }
+    private static byte setBitFromIndexToIndex(byte target, int startIndex, int endIndex) {
+        int mask = -1 << startIndex;
+        int clearMask = (1 << endIndex + 1) - 1;
+        mask &= clearMask;
+        return (byte) (target | mask);
     }
     private static byte setBitFromIndexToEnd(byte target, int bitIndex) {
         int mask = -1 << bitIndex;
@@ -45,21 +74,6 @@ public class DrawLine {
     private static byte setBitFromStartToIndex(byte target, int bitIndex) {
         int mask = (1 << (bitIndex + 1)) -1;
         return (byte) (target | mask);
-    }
-    private static byte setBitFromIndexToIndex(byte target, int startIndex, int endIndex) {
-        int mask = -1 << startIndex;
-        int clearMask = (1 << endIndex + 1) - 1;
-        mask &= clearMask;
-        return (byte) (target | mask);
-    }
-    private static boolean isOutOfBounds(int width, int x1, int x2) {
-        return x2 >= width || x2 < x1;
-    }
-    private static boolean isValid(int width, int x1, int x2, int y) {
-        return isDivisibleBy(width, 8) &&
-                x1 >= 0 &&
-                x2 >= 0 &&
-                y >= 0;
     }
     public static void printScreen(byte[] screen, int width) {
         int bitCount = 0;
@@ -80,9 +94,6 @@ public class DrawLine {
             current >>>= 1;
         }
         System.out.print(stringBuilder);
-    }
-    private static boolean isDivisibleBy(int dividend, int divisor) {
-        return dividend % divisor == 0;
     }
     public static void main(String[] args) {
         int size = 72;
